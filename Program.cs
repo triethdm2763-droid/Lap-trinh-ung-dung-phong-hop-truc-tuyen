72a7c54bb6b59897976b0e9552d95d82250383ae
Nhật Trường <truongpn5713@ut.edu.vn>
2026-03-26 19:02:56 +0700

    Add files via upload
    Add features: emoji, private chat, file transfer

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // ⭐ Fix lỗi tiếng Việt + emoji
+        Console.OutputEncoding = Encoding.UTF8;
+
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Xin chào 😄🔥❤️");
+
+        Console.ReadLine(); // giữ màn hình
+    }
+}
