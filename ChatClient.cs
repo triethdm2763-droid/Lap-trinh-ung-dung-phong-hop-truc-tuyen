cba5dd363d53f53278edeb1a38e23b61022e8dde
tuyenhnx0328-dev <tuyenhnx0328@ut.edu.vn>
2026-03-30 16:30:21 +0700

    Add files via upload

---
+++ ChatClient.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MeetingClient
+{
+    public partial class Form1 : Form
+    {
+        ChatClient client = new ChatClient();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            client.OnMessageReceived = DisplayMessage;
+        }
+
+        private void btnJoin_Click(object sender, EventArgs e)
+        {
+            client.ConnectToServer("127.0.0.1", 5000);
+
+            string name = txtName.Text;
+            string room = txtRoom.Text;
+
+            client.SendMessage($"JOIN|{name}|{room}");
+
+            MessageBox.Show("Joined!");
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            client.SendMessage(txtMessage.Text);
+            txtMessage.Clear();
+        }
+
+        void DisplayMessage(string msg)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action<string>(DisplayMessage), msg);
+                return;
+            }
+
+            rtbChat.AppendText(msg + "\n");
+        }
+    }
+}
