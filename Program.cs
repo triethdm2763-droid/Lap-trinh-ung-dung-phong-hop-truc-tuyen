c800228802453f1ebd09a6badddc54e4efa1c8d1
Nhật Trường <truongpn5713@ut.edu.vn>
2026-04-08 00:07:28 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using NetworkProgramming.Client;
+
+namespace NetworkProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClientForm client = new ClientForm();
+            client.Run();
+        }
+    }
+}
